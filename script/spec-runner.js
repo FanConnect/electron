b220806d0231a628ea00dfcf4cb5859d358e764a
John Kleinschmidt <jkleinsc@electronjs.org>
2025-08-29 17:04:15 -0400

    fixup

--- script/spec-runner.js
+++ script/spec-runner.js
@@ -338,7 +338,7 @@ async function runTestUsingElectron (specDir, testName, shouldRerun, additionalA
   }
   let argsToPass = unknownArgs.slice(2);
   if (additionalArgs.includes('--files')) {
-    argsToPass = argsToPass.filter(arg => (!arg.startsWith('--files') && !arg.startsWith('spec/')));
+    argsToPass = argsToPass.filter(arg => (arg.indexOf('--files') === -1 && arg.indexOf('spec/') === -1));
   }
   const runnerArgs = [`electron/${specDir}`, ...argsToPass, ...additionalArgs];
   if (process.platform === 'linux') {
